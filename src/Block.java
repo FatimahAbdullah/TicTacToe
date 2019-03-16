e608086c2e1f22cba3c51998f325951433dd4ab3
Fatimah <fatimah.a237@gmail.com>
2019-03-17 02:45:57 +0500

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -3,14 +3,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Block extends JButton implements ActionListener {
-    ImageIcon X,O;
+    ImageIcon X,O, none;
     byte value=0;
     public Block(){
-        X=new ImageIcon(this.getClass().getResource("X.png"));
-        O=new ImageIcon(this.getClass().getResource("O.png"));
+        X=new ImageIcon(this.getClass().getResource("tick.png"));
+        O=new ImageIcon(this.getClass().getResource("cancel.png"));
+        none=new ImageIcon(this.getClass().getResource("none.png"));
+        setIcon(none);
         this.addActionListener(this);
     }
-
     public void actionPerformed(ActionEvent e){
         value++;
         value%=3;
